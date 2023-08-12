cd2434c8bfd26ac0dc969602af651697dd0f551e
Cloud <cloud.frontier0@gmail.com>
2023-08-12 12:45:11 -0700

    change: deploy script

--- script/DeployCoinFlip.s.sol
+++ script/DeployCoinFlip.s.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.6.0;
 
 import "forge-std/Script.sol";
 import "../src/CoinFlip.sol";
@@ -10,7 +10,8 @@ contract DeployCoinFlip is Script {
     function run() external {
         uint256 callerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(callerPrivateKey);
-        coinFlipContract = CoinFlip(0xA7Bd64c7723b669D6ec63c799Eb97712026A546d);
+        coinFlipContract = new CoinFlip();
+        coinFlipContract.attack();
         vm.stopBroadcast();
     }
 }
